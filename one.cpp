e8e977d7b0cc9e8b18c884173f0d2a1646832b58
Roopesh-DMahanthi <deepthimahanthi.roopesh@gmail.com>
2020-02-21 01:02:20 +0530

    2nd com

--- one.cpp
+++ one.cpp
@@ -9,11 +9,23 @@ struct library
     vector<long int> books;
 };
 
+void showq(queue <long int> gq) 
+{ 
+    queue <long int> g = gq; 
+    while (!g.empty()) 
+    { 
+        cout << '\t' << g.front(); 
+        g.pop(); 
+    } 
+    cout << '\n'; 
+} 
+
 int main()
 {
     long int bks,libs,days,i=0,j;
     cin>>bks>>libs>>days;
     vector<long int> scr;
+
     for(i=0;i<bks;i++)
     {
         long int tp;
@@ -21,8 +33,11 @@ int main()
         scr.push_back(tp);
     }
 
-    vector<library> li;
+    
 
+    vector<library> li;
+    vector<long int> signupset;
+    queue<long int> order;
     for(i=0;i<libs;i++)
     {
         long int p1,p2,p3;
@@ -31,6 +46,7 @@ int main()
         n1.nob=p1;
         n1.sign=p2;
         n1.perd=p3;
+        signupset.push_back(p2);
         for(j=0;j<p1;j++)
         {
             long int tp;
@@ -39,6 +55,19 @@ int main()
         }
         li.push_back(n1);
     }
+
+    for(i=0;i<libs;i++)
+    {
+        long int indi=min_element(signupset.begin(),signupset.end())-signupset.begin();
+        signupset[indi]=INT_MAX;
+        //cout<<indi<<endl;
+        order.push(indi);
+    }
+
+    //showq(order);
+
+    //print
+    /*
     for(i=0;i<libs;i++)
     {
         cout<<li[i].nob<<" "<<li[i].sign<<" "<<li[i].perd<<endl;
@@ -48,4 +77,12 @@ int main()
         }
         cout<<endl;
     }
+    */    
+
+    vector<long int> slib;
+
+    while(days--){
+        
+    }
+    
 }
